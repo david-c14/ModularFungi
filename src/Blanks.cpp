d60c132c1b774387197f9ee7885dafa062ce26eb
carbon14 <10398567+david-c14@users.noreply.github.com>
2018-07-16 10:01:17 +0100

    Added support for bitmaps

--- src/Blanks.cpp
+++ src/Blanks.cpp
@@ -1,20 +1,75 @@
 #include "OC_Blanks.hpp"
 
+struct BitMap : TransparentWidget {
+	std::string path;
+	int loaded = false;
+	int bitmap;
+	void DrawImage(NVGcontext *vg) {
+		if (!loaded) {
+			loaded = true;
+			bitmap = nvgCreateImage(vg, path.c_str(), NVG_IMAGE_GENERATE_MIPMAPS);
+			if (!bitmap)
+				return;
+		
+			int w, h;
+			nvgImageSize(vg, bitmap, &w, &h);
+			box.size.x = w;
+			box.size.y = h;	
+			box.pos.x = (parent->box.size.x - w) / 2;
+			box.pos.y = (parent->box.size.y - h) / 2;
+			return;
+		}
+		if (!loaded)
+			return;	
+		NVGpaint paint = nvgImagePattern(vg, 0, 0, box.size.x, box.size.y, 0.0f, bitmap, 1.0f);
+		nvgFillPaint(vg, paint);
+		nvgBeginPath(vg);
+		nvgRect(vg, 0, 0, box.size.x, box.size.y);
+		nvgFill(vg);
+		
+	}
+	void draw(NVGcontext *vg) override {
+		DrawImage(vg);
+		TransparentWidget::draw(vg);
+	}
+};
+
 struct Blank1 : ModuleWidget {
 	Blank1(Module *module) : ModuleWidget(module) {
 		setPanel(SVG::load(assetPlugin(plugin, "res/Blank1.svg")));
+		addChild(Widget::create<ScrewSilver>(Vec(15, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x-30, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(15, 365)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x-30, 365)));
+		BitMap * bmp = Widget::create<BitMap>(Vec(0,0));
+		bmp->path = assetPlugin(plugin, "res/Blank1.png");
+		addChild(bmp);
 	}
 };
 
 struct Blank2 : ModuleWidget {
 	Blank2(Module *module) : ModuleWidget(module) {
 		setPanel(SVG::load(assetPlugin(plugin, "res/Blank2.svg")));
+		addChild(Widget::create<ScrewSilver>(Vec(15, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x-30, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(15, 365)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x-30, 365)));
+		BitMap * bmp = Widget::create<BitMap>(Vec(0,0));
+		bmp->path = assetPlugin(plugin, "res/Blank2.png");
+		addChild(bmp);
 	}
 };
 
 struct Blank3 : ModuleWidget {
 	Blank3(Module *module) : ModuleWidget(module) {
 		setPanel(SVG::load(assetPlugin(plugin, "res/Blank3.svg")));
+		addChild(Widget::create<ScrewSilver>(Vec(15, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x-30, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(15, 365)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x-30, 365)));
+		BitMap * bmp = Widget::create<BitMap>(Vec(0,0));
+		bmp->path = assetPlugin(plugin, "res/Blank3.png");
+		addChild(bmp);
 	}
 };
 
