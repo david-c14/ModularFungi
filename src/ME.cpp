40038254518ea0443daae802695aaf66c4c542a5
david-c14 <10398567+david-c14@users.noreply.github.com>
2018-09-01 13:30:20 +0100

    Further experiments with blending

--- src/ME.cpp
+++ src/ME.cpp
@@ -15,23 +15,28 @@ struct MEBitMap : TransparentWidget {
 		}
 		if (!bitmap)
 			return;	
-		NVGpaint paint = nvgImagePattern(vg, 0, 0, 60, box.size.y, 0.0f, bitmap, 1.0f);
+		NVGpaint paint1 = nvgImagePattern(vg, 0, 0, 60, box.size.y, 0.0f, bitmap, 1.0f);
+		NVGpaint paint2 = nvgImagePattern(vg, 0, 0, 30, box.size.y, 0.0f, bitmap, 1.0f);
 		nvgSave(vg);
-		nvgBeginPath(vg);
-		nvgRect(vg, 0, 0, box.size.x, box.size.y);
-		nvgFillColor(vg, nvgRGB(0, 0, 0));
-		nvgFill(vg);
 
 		nvgBeginPath(vg);
 		nvgRect(vg, 10, 10, 100, 100);
 		nvgFillColor(vg, nvgRGB(0xff, 0xff, 0xff));
 		nvgFill(vg);
-		nvgGlobalCompositeBlendFunc(vg, NVG_DST_COLOR, NVG_ZERO);
 
-		nvgFillPaint(vg, paint);
+		nvgGlobalCompositeBlendFunc(vg, NVG_DST_ALPHA, NVG_ZERO);
 		nvgBeginPath(vg);
 		nvgRect(vg, 0, 0, box.size.x, box.size.y);
+		nvgFillPaint(vg, paint1);
 		nvgFill(vg);
+
+		nvgGlobalCompositeBlendFunc(vg, NVG_ONE_MINUS_DST_ALPHA, NVG_ONE);
+		nvgBeginPath(vg);
+		nvgRect(vg, 0, 0, box.size.x, box.size.y);
+		nvgFillPaint(vg, paint2);
+		nvgFill(vg);
+		
+
 		nvgRestore(vg);
 		
 	}
@@ -48,12 +53,17 @@ struct MEBitMap : TransparentWidget {
 struct MEWidget : ModuleWidget {
 
 	MEWidget(Module *module) : ModuleWidget(module) {
-		box.size = Vec(RACK_GRID_WIDTH * 20, RACK_GRID_HEIGHT);
+		box.size = Vec(RACK_GRID_WIDTH * 20, RACK_GRID_HEIGHT * 2);
+		FramebufferWidget *fbw = Widget::create<FramebufferWidget>(Vec(0,0));
+		fbw->box.size.x = box.size.x;
+		fbw->box.size.y = box.size.y;
+		addChild(fbw);
 		MEBitMap * bmp = Widget::create<MEBitMap>(Vec(0,0));
 		bmp->box.size.x = box.size.x;
 		bmp->box.size.y = box.size.y;
 		bmp->path = assetPlugin(plugin, "res/Blank_4HP.png");
-		addChild(bmp);
+		fbw->addChild(bmp);
+		fbw->dirty = true;
 	}
 };
 
